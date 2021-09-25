8a10126e4403cbab162faebbf4dad1313f946f05
Ian Hunter <ianhunter@gmail.com>
2021-09-25 13:41:14 -0500

    pass all middleware thru

--- src/handler.ts
+++ src/handler.ts
@@ -2,11 +2,9 @@ import { Config } from "./types"
 import { buildServer } from "./server"
 
 function handler(config: Config = {}) {
-  const { schemas, extensions = [] } = config
-
   return async function handler(req, res) {
     if (req.method === "POST") {
-      const server = await buildServer({ schemas, extensions })
+      const server = await buildServer(config)
       const handler = server.createHandler({
         path: "/api/graphql",
       })
