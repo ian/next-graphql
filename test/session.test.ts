f4ca384d70b7513dc0250f97b240a5dc083c7a15
Ian Hunter <ianhunter@gmail.com>
2021-09-25 16:19:50 -0500

    session tests

---
+++ test/session.test.ts
@@ -0,0 +1,45 @@
+import testServer from "../.jest/server"
+
+describe("#session", () => {
+  it("should allow guarded endpoints", async () => {
+    const session = jest.fn(() => "SESSION OBJECT")
+    const { graphql } = await server(session)
+
+    const { errors, data } = await graphql(`
+      query {
+        session
+        secondEndpoint
+      }
+    `)
+
+    expect(session).toHaveBeenCalledTimes(1)
+    expect(errors).not.toBeDefined()
+    expect(data.session).toEqual("SESSION OBJECT")
+  })
+})
+
+const server = (session) => {
+  return testServer({
+    session,
+    extensions: [
+      () => ({
+        typeDefs: `
+        type Query {
+          session: String
+          secondEndpoint: String
+        }
+      `,
+        resolvers: {
+          Query: {
+            session: (obj, args, context) => {
+              return context.session
+            },
+            secondEndpoint: (bj, args, context) => {
+              return context.session
+            }
+          },
+        },
+      }),
+    ],
+  })
+}
