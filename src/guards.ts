dd2c4f3e459f62ebe2881b6f0551f41f15f92cca
Ian Hunter <ianhunter@gmail.com>
2021-09-29 16:38:29 -0500

    fallbackError

--- src/guards.ts
+++ src/guards.ts
@@ -2,33 +2,12 @@ import { shield } from "graphql-shield"
 import { Guards } from "./types"
 
 export function guardsMiddleware(guards: Guards) {
-  return shield(
-    guards,
-    {
-      fallbackError: "Error Occurred",
-    }
-    //   ,{
-    //   fallbackError: async (thrownThing, parent, args, context, info) => {
-    //     console.log({thrownThing, parent})
-    //     return new Error("Error Occurred")
-    //     // if (thrownThing instanceof ApolloError) {
-    //     //   // expected errors
-    //     //   return thrownThing
-    //     // } else if (thrownThing instanceof Error) {
-    //     //   // unexpected errors
-    //     //   console.error(thrownThing)
-    //     //   await Sentry.report(thrownThing)
-    //     //   return new ApolloError('Internal server error', 'ERR_INTERNAL_SERVER')
-    //     // } else {
-    //     //   // what the hell got thrown
-    //     //   console.error('The resolver threw something that is not an error.')
-    //     //   console.error(thrownThing)
-    //     //   return new ApolloError('Internal server error', 'ERR_INTERNAL_SERVER')
-    //     // }
-    //   },
-
-    // }
-  )
+  return shield(guards, {
+    fallbackError: async (thrownThing, parent, args, context, info) => {
+      console.error({ thrownThing, parent, args, info })
+      return new Error("Error Occurred")
+    },
+  })
 }
 
 export { rule, and, not, or, allow, deny } from "graphql-shield"
